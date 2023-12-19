89affcf6d27ef477f72e9a34fd62d118707cfedc
Terasail <91894024+Terasail@users.noreply.github.com>
2023-12-19 14:59:35 +0000

    Update to use table

--- filerequests.py
+++ filerequests.py
@@ -1,6 +1,9 @@
 import pywikibot
 import time
+import re
+import os
 
+os.system('cls')
 site = pywikibot.Site("en", "wikipedia")
 site.login()
 pages = []
@@ -8,21 +11,57 @@ categories = ["Wikipedia files requiring renaming", "Wikipedia files that shadow
 for catName in categories:
 	cat = pywikibot.Category(site, catName)
 	pages += list(cat.articles())
-wikitext = "There are currently no files waiting to be renamed."
-editSummary = "Update: No file rename requests"
-if len(pages) == 1:
-	wikitext = "There is currently 1 file waiting to be renamed:\n"
-	wikitext += "* [[:" + pages[0].title() + "]]\n"
-	editSummary = "Update: 1 file rename request"
-if len(pages) > 1:
-	wikitext = "There are currently " + str(len(pages)) + " files waiting to be renamed:\n"
-	for page in pages:
-		wikitext += "* [[:" + page.title() + "]]\n"
-	editSummary = "Update: " + str(len(pages)) + " file rename requests"
 
-wikitext = "{{Bots|deny=luckyrename}}\n" + wikitext.strip()
+editSummary = "Update: No file rename requests"
+wikitext = '\n{|class="wikitable" style="width:100%;text-align:center;"\n!File!!Target!!Reason'
+if len(pages) == 0:
+	wikitext = "There are currently no files waiting to be renamed." + wikitext + "\n|-\n|colspan=3|—"
+if len(pages) == 1:
+	editSummary = "Update: 1 file rename request"
+	wikitext = "There is currently 1 file waiting to be renamed:\n" + wikitext
+	wikitext += "\n|-\n|[[:" + pages[0].title() + "]]"
+	pageTemplates = pages[0].templatesWithParams()
+	search = True
+	for template in pageTemplates:
+		if (search):
+			if template[0].title() == "Template:ShadowsCommons":
+				search = False
+				wikitext += "||—||[[WP:FNC#9]]: File with the same name on Wikimedia Commons"
+			if template[0].title() == "Template:Rename media":
+				search = False
+				if len(template[1]) > 0:
+					wikitext += "||" + template[1][0]
+				else:
+					wikitext += "||—"
+				if len(template[1]) > 1:
+					wikitext += "||" + template[1][1]
+				else:
+					wikitext += "||—"
+if len(pages) > 1:
+	editSummary = "Update: " + str(len(pages)) + " file rename requests"
+	wikitext = "There are currently " + str(len(pages)) + " files waiting to be renamed:\n" + wikitext
+	for page in pages:
+		wikitext += "\n|-\n|[[:" + page.title() + "]]"
+		pageTemplates = page.templatesWithParams()
+		search = True
+		for template in pageTemplates:
+			if (search):
+				if template[0].title() == "Template:ShadowsCommons":
+					search = False
+					wikitext += "||—||[[WP:FNC#9]]: File with the same name on Wikimedia Commons"
+				if template[0].title() == "Template:Rename media":
+					search = False
+					if len(template[1]) > 0:
+						wikitext += "||" + template[1][0]
+					else:
+						wikitext += "||—"
+					if len(template[1]) > 1:
+						wikitext += "||" + template[1][1]
+					else:
+						wikitext += "||—"
+wikitext = "{{bots|allow=TeraBot}}\n" + wikitext + "\n|}"
 targetPage = pywikibot.Page(site, "User:TeraBot/FileRequests")
-if targetPage.text != wikitext:
-	print(time.strftime("%y/%m/%d %X", time.localtime()) + " | " + "Change")
-	targetPage.text = wikitext
+if re.sub("\n<sup>[^<]+</sup>", "", targetPage.text) != wikitext:
+	print(time.strftime("%y/%m/%d %X", time.localtime()) + " | " + editSummary)
+	targetPage.text = wikitext + "\n<sup>Last updated: " + time.ctime() + " (UTC)</sup>"
 	targetPage.save(summary=editSummary, minor=False)
