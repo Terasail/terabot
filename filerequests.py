f08dddb2022e4e77b72c411c6b633e71dfad64e0
Terasail <91894024+Terasail@users.noreply.github.com>
2023-12-27 02:32:26 +0000

    Update

--- filerequests.py
+++ filerequests.py
@@ -9,38 +9,37 @@ categories = ["Wikipedia files requiring renaming", "Wikipedia files that shadow
 for catName in categories:
 	cat = pywikibot.Category(site, catName)
 	pages += list(cat.articles())
-
 editSummary = "Update: "
-wikitext = '\n{|class="wikitable" style="width:100%;text-align:center;"\n!File!!Target!!Reason'
+wikitext = '\n{|class="wikitable sortable" style="width:100%;text-align:center;"\n!width=30%|File\n!width=30%|Target\n!Reason'
 if len(pages) == 0:
 	editSummary += "No file rename requests"
 	wikitext = "There are currently no files waiting to be renamed." + wikitext + "\n|-\n|colspan=3|—"
 if len(pages) > 0:
 	if len(pages) == 1:
 		editSummary += "1 file rename request"
-		wikitext = "There is currently 1 file waiting to be renamed:\n" + wikitext
+		wikitext = "There is currently 1 file waiting to be renamed:" + wikitext
 	else:
 		editSummary += str(len(pages)) + " file rename request"
-		wikitext = "There are currently " + str(len(pages)) + " files waiting to be renamed:\n" + wikitext
+		wikitext = "There are currently " + str(len(pages)) + " files waiting to be renamed:" + wikitext
 	for page in pages:
-		wikitext += "\n|-\n|[[:" + page.title() + "]]"
+		wikitext += "\n|-\n|[[:" + page.title() + "]]\n"
 		pageTemplates = page.templatesWithParams()
 		search = True
 		for template in pageTemplates:
 			if (search):
 				if template[0].title() == "Template:ShadowsCommons":
 					search = False
-					wikitext += "||—||[[WP:FNC#9]]: File with the same name on Wikimedia Commons"
+					wikitext += "|—\n|[[WP:FNC#9]]: File with the [[:commons:" + page.title() + "|same name]] on Wikimedia Commons"
 				if template[0].title() == "Template:Rename media":
 					search = False
 					if len(template[1]) > 0:
-						wikitext += "||[[:File:" + template[1][0] + "]]"
+						wikitext += "|[[:File:" + template[1][0] + "]]"
 					else:
-						wikitext += "||—"
+						wikitext += "|—"
 					if len(template[1]) > 1:
-						wikitext += "||" + template[1][1]
+						wikitext += "\n|" + template[1][1]
 					else:
-						wikitext += "||—"
+						wikitext += "\n|—"
 wikitext = "{{bots|allow=TeraBot}}\n" + wikitext.replace("File:File:", "File:") + "\n|}"
 targetPage = pywikibot.Page(site, "User:TeraBot/FileRequests")
 if re.sub("\n<sup>[^<]+</sup>", "", targetPage.text) != wikitext:
